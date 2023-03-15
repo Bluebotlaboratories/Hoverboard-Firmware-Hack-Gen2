70b0dedb06ea9825524a5a5a46528b1f3b132df1
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2023-03-15 08:10:29 +0000

    Merge PlatformIO support

--- HoverBoardGigaDevice/Inc/config.h
+++ HoverBoardGigaDevice/Inc/config.h
@@ -36,12 +36,12 @@
 // ################################################################################
 
 //#define MASTER										  	// Select if firmware is for master or slave board
-#define SLAVE 												// Select if firmware is for master or slave board
+#define SLAVE 					ls							// Select if firmware is for master or slave board
 
 // ################################################################################
 
 #define PWM_FREQ         		16000     // PWM frequency in Hz
-#define DEAD_TIME        		60        // PWM deadtime (60 = 1µs, measured by oscilloscope)
+#define DEAD_TIME        		60        // PWM deadtime (60 = 1ï¿½s, measured by oscilloscope)
 
 #define DC_CUR_LIMIT     		15        // Motor DC current limit in amps
 
@@ -56,13 +56,13 @@
 
 // ################################################################################
 
-#define BAT_LOW_LVL1     35.0       // Gently beeps, show green battery symbol above this Level.
-#define BAT_LOW_LVL2     33.0       // Battery almost empty, show orange battery symbol above this Level. Charge now! 
-#define BAT_LOW_DEAD     31.0       // Undervoltage lockout, show red battery symbol above this Level.
+//#define BAT_LOW_LVL1     35.0       // Gently beeps, show green battery symbol above this Level.
+//#define BAT_LOW_LVL2     33.0       // Battery almost empty, show orange battery symbol above this Level. Charge now! 
+//#define BAT_LOW_DEAD     31.0       // Undervoltage lockout, show red battery symbol above this Level.
 // ONLY DEBUG-LEVEL!!!
-//#define BAT_LOW_LVL1     29.0
-//#define BAT_LOW_LVL2     28.0
-//#define BAT_LOW_DEAD     27.0
+#define BAT_LOW_LVL1     28.0
+#define BAT_LOW_LVL2     27.0
+#define BAT_LOW_DEAD     26.0
 
 // ################################################################################
 #endif
